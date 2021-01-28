c94ac2cf4cc3369fd97c33e3c96ccdc96ffc51de
Lurzapps <lurzapps@gmail.com>
2021-01-28 11:09:22 +0100

    inform user about whats happening

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ import ctypes
 # define utc
 utc = pytz.UTC
 
+# inform user what is happening
+os.system('title wallpaper swap')
+
 # get public ip address
+print('getting public ip')
 ip = req.get('https://api.ipify.org').text
+
 # request lat / lng
-jsonLatLng = req.get('http://ip-api.com/json/' + ip).json()
+print('extracting latitude and longitude from ip')
+json_lat_lng = req.get('http://ip-api.com/json/' + ip).json()
 # extract lat / lng
-lat, lng = jsonLatLng['lat'], jsonLatLng['lon']
+lat, lng = json_lat_lng['lat'], json_lat_lng['lon']
+
 # request sunrise and sunset as json
-ssJson = req.get('https://api.sunrise-sunset.org/json', params={'lat': lat, 'lng': lng}).json()['results']
+print('requesting sunrise and sunset time')
+json_ss = req.get('https://api.sunrise-sunset.org/json', params={'lat': lat, 'lng': lng}).json()['results']
 # extract sunrise and sundown (in utc)
-sunrise, sunset = ssJson['sunrise'], ssJson['sunset']
+sunrise, sunset = json_ss['sunrise'], json_ss['sunset']
 
 # current time (localize call necessary because python thinks this time is unaware)
 now = utc.localize(datetime.now())
@@ -33,6 +41,7 @@ day_start, day_end = to_datetime(sunrise), to_datetime(sunset)
 is_bright = day_start <= now <= day_end
 
 # if is dark, set night wallpaper, else one of day wallpapers
+print('setting random wallpaper, is_bright = %r' % is_bright)
 # determine absolute image path
 dir_name = 'day' if is_bright else 'night'
 path = os.path.abspath(dir_name + '/' + random.choice(os.listdir(dir_name)))
